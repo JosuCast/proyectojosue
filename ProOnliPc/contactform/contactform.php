814f620b97bb8cd471787a79ac2e44ecf38fa77e
Josue9876 <86072865+Josue9876@users.noreply.github.com>
2022-11-03 12:36:20 -0500

    Update contactform.php

--- ProOnliPc/contactform/contactform.php
+++ ProOnliPc/contactform/contactform.php
@@ -1,28 +1,19 @@
 <?php 
-
 $nombre = $_POST['name'];
 $mail = $_POST['email'];
 $telefono = $_POST['subject'];
-$empresa = $_POST['message'];
+$mensaje = $_POST['message'];
 
-$header = 'from: '.$mail."\r\n";
-$header .= "X-Mailer:PHP/".phpversion()."\r\n";
-$header .= "Mime-Version:1.0 \r\n";
-$header .= "content-Type:text/plain";
+$header = "Enviado desde la pagina web";
+$mensajecompleto= $mensaje. "\nAtentamente: " . $nombre. "\nTelefono: ". $telefono;
 
-$message = "este message fue enviado por: ".$name."\r\n";
-$message .= "Su e-mail es: ".$mail."\r\n";
-$message .= "Telefono: ".$subject."\r\n";
-$message .= "message: ".$_POST['message']."\r\n";
-$message .= "enviado el: ".date('d/m/Y',time());
 
 
 $para = 'josue.castillo@tecsup.edu.pe';
-$asunto = 'message de mi web';
 
-if(mail($para,$asunto,utf8_decode($message),$header))
+
+mail($para,$asunto,$mensajecompleto,$header);
 echo "<script type='text/javascript'>alert('Tu message ha sido enviado exitosamente');</script>";
 echo "<script type='text/javascript'>window.location.href='https://proyectojosue1.herokuapp.com';</script>";
-
 
  ?>
